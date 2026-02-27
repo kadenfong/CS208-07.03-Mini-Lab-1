b7be4b238d8955026bce4d67fea39eb02345dd6a
kadenfong <kadenfong@u.boisestate.edu>
2026-02-27 03:01:49 +0000

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function calcWordFrequencies(words) {
+    let list = words.split(" ");
+    let counts = new Map();
 
+    for (let item of list) {
+        if (counts.has(item)) {
+            counts.set(item, counts.get(item) + 1);
+        }
+        else {
+            counts.set(item, 1);
+        }
+    }
+
+    for (let [name, total] of counts) {
+        console.log(name + " " + total);
+    }
 }
 
 
